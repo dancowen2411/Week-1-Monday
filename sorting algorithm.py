2527ec1eb00212899a1765d6260e509771cb0a2d
Daniel Cowen <dancowen2411@gmail.com>
2026-02-16 11:20:50 +0000

    i fixed it

--- sorting algorithm.py
+++ sorting algorithm.py
@@ -1,20 +1,4 @@
-import numpy as np
-import matplotlib
-from random import randint
-from timeit import repeat
-
-def run_sorting_algorithm(algorithm, array):
-    setup_code = f"from __main__ import {algorithm}" \
-        if algorithm != "sorted" else ""
-
-    stmt = f"{algorithm}({array})"
-    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
-
-    print()
-    
-
 def ListSorter(array):
-    sortedlist = [1]
     n = len(array)
     for x in range(n):
         already_sorted = True
@@ -28,4 +12,6 @@ def ListSorter(array):
    
 if __name__ == "__main__":
     array = [5,2,1,6,2,3,9,7,8,11]
-    run_sorting_algorithm(algorithm="ListSorter", array=array)
+    x=ListSorter(array)
+    print(x)
+    
